8f5f81895838c783df22e672b9af1c303a0e0719
sboevvitalii <140063683+sboevvitalii@users.noreply.github.com>
2024-01-11 11:14:39 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import '../index.css';
+import JS_IMAGE from '../assets/jawascript.png';
+
+
+const jsImageHTML = document.createElement('img');
+jsImageHTML.src = JS_IMAGE;
+
+document.body.append(jsImageHTML);
